83e2a0f9ea17afec3670e9fb8b215b61b3b97817
DJ Tape <Dj.Tapee@yandex.ru>
2024-06-03 01:34:13 +0300

    Set base project structure

---
+++ Lab 5/MyContainers/MySet.hpp
@@ -0,0 +1,10 @@
+#ifndef LAB_5_MYSET_HPP
+#define LAB_5_MYSET_HPP
+
+
+class MySet {
+
+};
+
+
+#endif //LAB_5_MYSET_HPP
---
+++ Lab 5/MyContainers/MyVector.hpp
@@ -0,0 +1,10 @@
+#ifndef LAB_5_MYVECTOR_HPP
+#define LAB_5_MYVECTOR_HPP
+
+
+class MyVector {
+
+};
+
+
+#endif //LAB_5_MYVECTOR_HPP
