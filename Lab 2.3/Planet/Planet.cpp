ad869261ccc0f8965e53ceff6413d0ab006ac8a4
DJ Tape <Dj.Tapee@yandex.ru>
2024-06-04 13:03:10 +0300

    Set base project structure

---
+++ Lab 2.3/Planet/Planet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by DJ Tape on 04.06.2024.
+//
+
+#include "Planet.hpp"
---
+++ Lab 2.3/Planet/Planet.hpp
@@ -0,0 +1,10 @@
+#ifndef LAB_2_3_PLANET_HPP
+#define LAB_2_3_PLANET_HPP
+
+
+class Planet {
+
+};
+
+
+#endif //LAB_2_3_PLANET_HPP
