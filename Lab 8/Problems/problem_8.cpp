179d61e12bd2e78172c42dc4b218d5effebcfd08
DJ Tape <Dj.Tapee@yandex.ru>
2024-06-04 07:49:25 +0300

    Solved problem 8

---
+++ Lab 8/Problems/problem_8.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <map>
+
+
+void PrintMap(const std::map<int, double> &map) {
+	for (const auto& [integer, decimal] : map) {
+		std::cout << integer << ": " << decimal << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+void DeleteEven(std::map<int, double> &map) {
+	for (auto& [key, value] : map) {
+		if (!(key % 2)) {
+			map.erase(key);
+		}
+	}
+}
+
+int main() {
+	std::map<int, double> map {
+			{1, 1.1},
+			{2, 2.2},
+			{3, 3.3},
+			{4, 4.4},
+			{5, 5.5},
+			{6, 6.6},
+			{7, 7.7},
+			{8, 8.8},
+			{9, 9.9},
+	};
+	PrintMap(map);
+	DeleteEven(map);
+	PrintMap(map);
+}
